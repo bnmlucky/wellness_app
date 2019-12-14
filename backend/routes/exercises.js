562e85237624a6cc495c9072a676237e0ca539ab
Natalia Titova <bnmlucky@gmail.com>
2019-12-13 21:35:27 -0500

    created show, delete and update routes for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,23 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+
+        })
+})
+
 module.exports = router;
